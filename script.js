a627aea86e0777cf70796decb308a996611691f7
NeNaZIcK <mprozasm@gmail.com>
2025-12-09 21:52:51 +0100

    Afegida funcionalitat

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ console.log("Projecte ModaGlobal carregat correctament");
 document.getElementById("btnProva").addEventListener("click", () => {
     alert("Funcionalitat bàsica activada");
 });
+
+console.log('Nova funcionalitat afegida');
